1c8f6f17123fda1cd2e4e2674c66aa970fbff61a
niral3737 <niral.eagle100@gmail.com>
2016-06-06 18:11:00 +0530

    nav drawer

--- app/src/main/java/com/itgo/motorapp/ActivityMain.java
+++ app/src/main/java/com/itgo/motorapp/ActivityMain.java
@@ -3,11 +3,14 @@ package com.itgo.motorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
 public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DrawerBuilder().withActivity(this).build();
     }
 }
